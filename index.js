9f58020ee2009e7cfd5a7ccb59fbdc54417e57c7
Rodrigo Sturm <rodristurm@gmail.com>
2023-12-06 00:25:28 -0300

    cambie archivo main de vuelta

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 /*
 Ejemplo hecho con broker EMQX
 */
+import express from 'express'
+const app = express()
 
+app.listen(3000)
+console.log('Server en puerto 3000')
+
+app.get('/', (req, res)=>{
+  res.send('Hola server')
+})
 
 /*
 const mqtt = require('mqtt')
--- src/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const app = express()
-
-app.listen(3000)
-console.log('Server en puerto 3000')
-
-app.get('/', (req, res)=>{
-  res.send('Hola server')
-})
