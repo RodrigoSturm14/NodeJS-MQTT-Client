cf67ae29a73267c95d41cf47318eaee4e57595a4
Rodrigo Sturm <rodristurm@gmail.com>
2023-12-06 00:03:52 -0300

    saque codigo

--- index.js
+++ index.js
@@ -2,18 +2,6 @@
 Ejemplo hecho con broker EMQX
 */
 
-const http = require('http');
-const PORT = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
 
 /*
 const mqtt = require('mqtt')
